d2550f5372922e5d2fba315e9fab9825d9eacb5b
jyoti-mishra32 <kumarijyotimishra264@gmail.com>
2026-09-15 16:28:38 +0530

    Create script.js

---
+++ Interactive Developer Landing Page/script.js
@@ -0,0 +1,9 @@
+const circle = document.querySelector(".circle");
+
+document.addEventListener("mousemove", (e) => {
+    circle.style.left = e.clientX + "px";
+    circle.style.top = e.clientY + "px";
+});
+
+
+
